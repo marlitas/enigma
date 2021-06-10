c0e5e185375fe47a22bb282f723e1b445ee697a0
Marla Schulz <marlaa.schulz@gmail.com>
2021-06-10 16:37:47 -0600

    Finish testing at current level

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe Enigma do
   it 'exists' do
     expect(@enigma).to be_a Enigma
   end
+
+  it 'can encrypt' do
+    expect(@enigma.encrypt('hello world', '02715', '040895')).to eq({ encryption: 'keder ohulw', key: '02715', date: '040895'})
+  end
+
+  it 'can decrypt' do
+    expect(@enigma.decrypt('keder ohulw', '02715', '040895')).to eq({decryption: 'hello world', key: '02715', date: '040895'})
+  end
+  
 end
