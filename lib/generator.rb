b4f61fd1f1bbfb74875e1d20601dbfe9462ca064
Marla Schulz <marlaa.schulz@gmail.com>
2021-06-10 18:04:56 -0600

    Start generator

---
+++ lib/generator.rb
@@ -0,0 +1,16 @@
+require 'date'
+
+class Generator
+  attr_reader :character_set
+  def initiliaze
+    @character_set = ("a".."z").to_a << " "
+  end
+
+  def key
+
+  end
+
+  def split_key(key)
+
+  end
+end
