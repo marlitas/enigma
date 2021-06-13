a0cc8cf6a25961613670b39c3ce265b499d9a5e7
Marla Schulz <marlaa.schulz@gmail.com>
2021-06-13 16:19:21 -0600

    Complete date method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -7,12 +7,20 @@ class Enigma
   end
 
   def encrypt(message, key = @generator.key, date = Date.new)
-    outgoing_hash = {message: message, key: key, date: date}
+    outgoing_hash = {message: message, key: key, date: date_convert(date)}
     @generator.encrypt_code(outgoing_hash)
   end
 
   def decrypt(cipher, key, date = Date.new)
-    outgoing_hash = {cipher: cipher, key: key, date: date}
+    outgoing_hash = {cipher: cipher, key: key, date: date_convert(date)}
     @generator.decrypt_code(outgoing_hash)
+  end
+
+  def date_convert(date)
+    if date.class == Date
+      date.strftime('%d%m%y')
+    else
+      date
+    end
   end
 end
