a1baf22e49530b3174f385db8f9133465a6491cc
Marla Schulz <marlaa.schulz@gmail.com>
2021-06-13 12:44:46 -0600

    Unskip enigma

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,21 +1,27 @@
 require './lib/enigma'
+require './lib/generator'
 require 'date'
 
 RSpec.describe Enigma do
   before(:each) do
-    @enigma = Enigma.new
+    @generator = Generator.new
+    @enigma = Enigma.new(@generator)
   end
 
   it 'exists' do
     expect(@enigma).to be_a Enigma
   end
 
+  it 'has attributes' do
+    expect(@engima.generator).to be_a(Generator)
+  end
+
   it 'can encrypt' do
     expect(@enigma.encrypt('hello world', '02715', '040895')).to eq({ encryption: 'keder ohulw', key: '02715', date: '040895'})
   end
 
-  it 'can decrypt' do
+  xit 'can decrypt' do
     expect(@enigma.decrypt('keder ohulw', '02715', '040895')).to eq({decryption: 'hello world', key: '02715', date: '040895'})
   end
-  
+
 end
