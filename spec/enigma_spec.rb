777c94e0c9c5efb3999263274a01b8baa43bb104
Marla Schulz <marlaa.schulz@gmail.com>
2021-06-13 13:04:14 -0600

    Unskip decrypt method test

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe Enigma do
     expect(@enigma.encrypt('hello world', '02715', '040895')).to eq({ encryption: 'keder ohulw', key: '02715', date: '040895'})
   end
 
-  xit 'can decrypt' do
+  it 'can decrypt' do
     expect(@enigma.decrypt('keder ohulw', '02715', '040895')).to eq({decryption: 'hello world', key: '02715', date: '040895'})
   end
 
